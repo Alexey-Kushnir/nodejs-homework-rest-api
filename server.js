3c8c6b5023289debf19eb860098d933bb639299a
Alexey-Kushnir <al7787@gmail.com>
2023-05-24 12:59:20 +0300

    add .env

--- server.js
+++ server.js
@@ -4,18 +4,13 @@ const app = require("./app");
 
 const { DB_HOST } = process.env;
 
-// const { DB_HOST } = require("./config");
-// console.log(process.env.DB_HOST);
-
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
+    app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
