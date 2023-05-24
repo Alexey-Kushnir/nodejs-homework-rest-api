c909c23ea2f1992aa25ef1e02209bb423be521fd
Alexey-Kushnir <al7787@gmail.com>
2023-05-24 13:21:53 +0300

    try async/await

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     console.log("Database connection successful");
+//     app.listen(PORT);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
+
+async function startServer() {
+  try {
+    await mongoose.connect(DB_HOST);
     console.log("Database connection successful");
     app.listen(PORT);
-  })
-  .catch((error) => {
+    console.log(`Server started on port ${PORT}`);
+  } catch (error) {
     console.log(error.message);
     process.exit(1);
-  });
+  }
+}
+
+startServer();
